99f26be1924a6b611d6128f0fe620884d4a4033a
xSquishyLiam <56977759+xSquishyLiam@users.noreply.github.com>
2026-05-13 01:42:58 +0100

    updated to 26.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    implementation("dev.jorel:commandapi-paper-shade:11.1.0")
+    implementation("dev.jorel:commandapi-paper-shade:11.2.0")
 
     compileOnly("me.clip:placeholderapi:2.12.2")
 
